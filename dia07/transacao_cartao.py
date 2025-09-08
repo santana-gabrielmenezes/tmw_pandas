6744b058685fcc510fdaec87624f19f8992b658c
santana.gabrielmenezes <santana.gabrielmenezes+github@gmail.com>
2025-09-08 12:48:53 -0300

    fim do curso de pandas

--- dia07/transacao_cartao.py
+++ dia07/transacao_cartao.py
@@ -17,11 +17,21 @@ df.explode('ValorParcela')
 # %%
 df_fatura = df.explode('ValorParcela')
 df_fatura = df_fatura.drop(['Valor','Parcelas'], axis=1)
+
 df_fatura['Months_add'] = df_fatura.groupby('idTransaction')['dtTransaction'].rank('first').astype(int)
-df_fatura.apply(add_months, axis=1)
+
+def add_months(row) :
+    new_date =  row['dtTransaction'] + np.timedelta64(row['Months_add']*30, 'D')
+    dt_str = '-'.join(str(new_date).split('-')[:-1]) +'-01'
+    return dt_str
+
+df_fatura['DtFatura'] = df_fatura.apply(add_months, axis=1)
 df_fatura
 # %%
-def add_months(row) :
-    new_date =  row['dtTransaction'] + np.timedelta64(row['Months_add'], 'M')
-    return new_date
+df_fatura = df_fatura.groupby(['idCliente','DtFatura'])['ValorParcela'].sum()
+df_fatura = df_fatura.reset_index()
+# %%
+fatura_mes = df_fatura.pivot_table(columns='DtFatura', index='idCliente',values='ValorParcela')
+fatura_mes = fatura_mes.fillna(0)
+fatura_mes
 # %%
